d01e96d24934dca75b267a6108c9ee4fb0493d21
Fredy Carcuz <fcarcuz@vm.local>
2017-10-31 16:37:20 -0600

    validating organization content

--- common/legalcontent.js
+++ common/legalcontent.js
@@ -4,10 +4,12 @@ var LegalContent = function(){
 	this.legalContainer = element(by.css('.kt-legal-container'));
 	this.titles = element.all(by.tagName('h2'));
 	this.title1 = this.titles.get(1).getText();
-	this.titleLast = this.titles.last().getText();	
+	this.titleLast = this.titles.last().getText();
+	if(browser.params.site.page==='organizations/'){	
 	this.organizationList = element.all(by.tagName('li'));
 	this.organization1 = this.organizationList.get(0).getText();
 	this.organizationLast = this.organizationList.get(25).getText();//some random #
+	}
 }
 
 module.exports = LegalContent;
